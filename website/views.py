651054570b425ab729ae8ec7111cec11c9c04739
Bukola Omotoso <bgo2e@mtmail.mtsu.edu>
2020-05-21 06:36:03 -0500

    Created a Template

--- website/views.py
+++ website/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 from datetime import datetime
 # Create your views here.
 def welcome(request):
-    return HttpResponse("Welcome to Meeting Planner")
+    #return HttpResponse("Welcome to Meeting Planner")
+    return render(request, "website/welcome.html")
+
 
 def date(request):
     return HttpResponse("This page was served at " +str(datetime.now()))
